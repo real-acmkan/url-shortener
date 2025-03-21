1d0c994e70d941944b8a40266d18ab9d8b47bb56
Sebastien Fnt <57267809+Cofer07@users.noreply.github.com>
2025-03-20 22:26:16 -0300

    Add files via upload

--- db_templates/tinyurl_db_passwordresets.sql
+++ db_templates/tinyurl_db_passwordresets.sql
@@ -28,9 +28,9 @@ CREATE TABLE `passwordresets` (
   `token` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_user_reset` (`user_id`),
   KEY `fk_passwordresets_user` (`user_id`),
-  CONSTRAINT `fk_passwordresets_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `passwordresets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+  CONSTRAINT `fk_passwordresets_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
